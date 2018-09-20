4e327274099039f43b58dac3c5baa87f1fdcb99b
kzhen7923 <kaitlyn.zhen21@myhunter.cuny.edu>
2018-09-19 20:36:16 -0400

    pig latin

---
+++ hw_02/README.md
@@ -0,0 +1,1 @@
+Kaitlyn Zhen and Shanjida Kamal
---
+++ hw_02/pig.py
@@ -0,0 +1,15 @@
+def piglatinify(word):
+    first_letter = word[:1]
+    rest_word = word[1:]
+    
+    if first_letter in "aeiou":
+        print(word + "ay")
+        
+    else:
+        print(rest_word + first_letter + "ay")
+        
+piglatinify("apple")
+piglatinify("banana")
+piglatinify("elephant")
+piglatinify("ink")
+piglatinify("movie")
