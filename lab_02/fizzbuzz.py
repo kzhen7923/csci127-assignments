e6fdab0eb0bc8549a93e12c6cee952f53cffcb6d
kzhen7923 <kaitlyn.zhen21@myhunter.cuny.edu>
2018-09-24 08:48:52 -0400

    added lab-02

---
+++ lab_02/fizzbuzz.py
@@ -0,0 +1,20 @@
+#Kaitlyn Zhen and Matthew Barron
+
+def fizzbuzz(max_value):
+
+    i = 0
+    count = 0
+    while i <= max_value-1:
+        i = i + 1
+        if i % 3 == 0 and i % 5 != 0:
+            print("fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+            count = count + 1
+        else:
+            print(i)
+    return count 
+
+print(fizzbuzz(100))
