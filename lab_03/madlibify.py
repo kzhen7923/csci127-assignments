edc134d17eda0b6cc3113096e30b8254b7349c88
kzhen7923 <kaitlyn.zhen21@myhunter.cuny.edu>
2018-10-18 08:28:03 -0400

    add lab_03

--- lab_03/madlibify.py
+++ lab_03/madlibify.py
@@ -2,26 +2,29 @@
 
 import random
 
-exclamations = ["ouch!", "zoinks!", "jinkies!"]
-adverbs = ["quickly", "amazingly", "sadly"]
+exclamations = ["'rub-roh,raggy!'", "'zoinks!'", "'jinkies!'"]
+adverbs = ["quickly", "amusingly", "sadly"]
 nouns = ["dog", "hammer", "cat", "car", "frog"]
-adjectives = ["tiny", "questionable", "smelly"]
+adjectives = ["cowardly", "questionable", "smelly"]
 verbs = ["ate", "walked", "slept"]
+heroes = ["Flash", "Robin", "Thor"]
 
 paragraph1 = "<EXCLAMATION> he said <ADVERB> as he jumped into his <NOUN> and drove off with his <ADJECTIVE> wife."
 paragraph2 = "Sam <VERB> the <NOUN> and then <VERB> the <NOUN> later"
+paragraph3 = "<HERO> <VERB> in the <NOUN> and then <HERO> <VERB> later."
 
 
-def choose_random(l):
+def choose_random(l): #chooses random element in list
     return l[random.randrange(0, len(l))]
 
-def stringify(l):
+def stringify(l):   # turns list into string
     return " ".join(l)
 
 def madlibify(paragraph):
-    word_list = paragraph.split()
-    madlib_list = []
-    for item in word_list:
+    word_list = paragraph.split()  # split the paragraph into a list
+    madlib_list = []  # list of words that madlib paragraph will use
+    hero = choose_random(heroes)  # makes one unique hero
+    for item in word_list:  # check through each word
         if item == "<EXCLAMATION>":
             madlib_list.append(choose_random(exclamations))
         elif item == "<ADVERB>":
@@ -32,10 +35,13 @@ def madlibify(paragraph):
             madlib_list.append(choose_random(adjectives))
         elif item == "<VERB>":
             madlib_list.append(choose_random(verbs))
+        elif item == "<HERO>":
+            madlib_list.append(hero)
         else:
             madlib_list.append(item)
-    madlib_paragraph = stringify(madlib_list)
+    madlib_paragraph = stringify(madlib_list)  # combine list of words back to paragraph
     return madlib_paragraph
 
 print(madlibify(paragraph1))
 print(madlibify(paragraph2))
+print(madlibify(paragraph3))
